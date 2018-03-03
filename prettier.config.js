f787bba3c04b158f1ef21c2487be7469701396e1
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2018-03-04 08:11:14 +0900

    setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 90,
+  parser: 'typescript',
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: false
+};
